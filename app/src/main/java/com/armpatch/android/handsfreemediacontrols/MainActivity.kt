1b71c4ae036a2b8ef225c90a16ce4feae684cc49
Aaron <ampatch@gmail.com>
2020-03-21 08:30:23 -0400

    connected proximity sensor with output near/far to text view in activity

--- app/src/main/java/com/armpatch/android/handsfreemediacontrols/MainActivity.kt
+++ app/src/main/java/com/armpatch/android/handsfreemediacontrols/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.armpatch.android.handsfreemediacontrols
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var proximitySensor: Sensor
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text)
+
+        setupProximitySensor()
+    }
+
+    private fun setupProximitySensor() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_UI
+        )
+    }
+
+
+    private var proximitySensorEventListener: SensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    textView.text = "Near"
+                } else {
+                    textView.text = "far"
+                }
+            }
+        }
     }
 }
