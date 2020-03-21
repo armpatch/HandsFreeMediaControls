bc3d9f399fe75358e9b2371f85eb2d8b86d4f5b7
Aaron <ampatch@gmail.com>
2020-03-21 07:46:34 -0400

    initial commit with empty activity

---
+++ app/src/main/java/com/armpatch/android/handsfreemediacontrols/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.armpatch.android.handsfreemediacontrols
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
