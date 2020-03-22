dd6b0dffbb1f5ea98d4c4d252e3cf0987cd806e2
Aaron <ampatch@gmail.com>
2020-03-22 18:49:32 -0400

    created a proximity sensor class to move logic out of MainPresenter

--- app/src/main/java/com/armpatch/android/handsfreemediacontrols/MainPresenter.kt
+++ app/src/main/java/com/armpatch/android/handsfreemediacontrols/MainPresenter.kt
@@ -2,22 +2,16 @@ package com.armpatch.android.handsfreemediacontrols
 
 import android.app.Activity
 import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.media.AudioManager
 import android.view.KeyEvent
 
 class MainPresenter(view: MainContract.View, private val activityContext: Context)
-    : MainContract.Presenter {
+    : MainContract.Presenter, ProximitySensor.ProximityListener {
 
     private var view: MainContract.View? = view
 
     private lateinit var audioManager: AudioManager
-
-    private lateinit var sensorManager: SensorManager
-    private lateinit var proximitySensor: Sensor
+    private lateinit var proximitySensor: ProximitySensor
 
     override fun onDestroy() {
         this.view = null
@@ -25,44 +19,19 @@ class MainPresenter(view: MainContract.View, private val activityContext: Contex
 
     override fun onCreate() {
         setupAudioManager()
-        setupProximitySensor()
+        proximitySensor = ProximitySensor(activityContext, this)
     }
 
     private fun setupAudioManager() {
         audioManager = activityContext.getSystemService(Activity.AUDIO_SERVICE) as AudioManager
     }
 
-    private fun setupProximitySensor() {
-        sensorManager = activityContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
-
-        sensorManager.registerListener(
-            proximitySensorEventListener,
-            proximitySensor,
-            SensorManager.SENSOR_DELAY_UI
-        )
-    }
-
-    private var proximitySensorEventListener: SensorEventListener = object : SensorEventListener {
-        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
-
-        override fun onSensorChanged(event: SensorEvent) {
-            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
-                if (event.values[0] == 0f) {
-                    onCloseProximity()
-                } else {
-                    onFarProximity()
-                }
-            }
-        }
-    }
-
-    private fun onCloseProximity() {
+    override fun onCloseProximity() {
         view?.cycleToNextActionIcon()
         //toggleMediaPlayPause()
     }
 
-    private fun onFarProximity() {
+    override fun onFarProximity() {
         view?.selectCurrentActionIcon()
     }
 
---
+++ app/src/main/java/com/armpatch/android/handsfreemediacontrols/ProximitySensor.kt
@@ -0,0 +1,49 @@
+package com.armpatch.android.handsfreemediacontrols
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+
+class ProximitySensor(private val activityContext: Context, private val listener: ProximityListener) {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var proximitySensor: Sensor
+    private val proximityEventListener: SensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0] == 0f) {
+                    listener.onCloseProximity()
+                } else {
+                    listener.onFarProximity()
+                }
+            }
+        }
+    }
+
+    interface ProximityListener {
+        fun onCloseProximity()
+        fun onFarProximity()
+    }
+
+    init {
+        setupProximitySensor()
+    }
+
+    private fun setupProximitySensor() {
+        sensorManager = activityContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+
+        sensorManager.registerListener(
+            proximityEventListener,
+            proximitySensor,
+            SensorManager.SENSOR_DELAY_UI
+        )
+    }
+
+
+}
